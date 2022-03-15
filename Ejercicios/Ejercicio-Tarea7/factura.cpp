e85bbf0d22d6071618ff686c2af9786cf4818181
Belkis Tejada <ptejada153@gmail.com>
2022-03-15 11:47:18 -0700

    Agregando Ejercicio Tarea7

---
+++ Ejercicios/Ejercicio-Tarea7/factura.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "factura.h"
+
+using namespace std;
+
+double Subtotal, Total;
+string listaProductos;
+double Impuesto;
+
+
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+
+{
+    listaProductos= listaProductos + descripcion + '\n';
+    Subtotal = Subtotal + (cantidad * precio);
+    Impuesto= Subtotal * 0.15;
+    Total = Subtotal + Impuesto;
+
+
+}
+void ImprimirFactura()
+
+{
+    system ("cls");
+    cout << "*******" << endl;
+    cout << "FACTURA" << endl;
+    cout << "*******" << endl;
+    cout << endl;
+
+    cout <<"Productos:"<< endl;
+    cout << listaProductos;
+
+    cout << endl;
+    cout << "Subtotal:" << Subtotal;
+
+    cout << endl;
+    cout << "Impuesto:" << Impuesto;
+
+    cout << endl;
+    cout << "Total a Pagar:" << Total;
+
+
+    cout << endl;
+    cout << endl;
+    system("pause");
+
+}
---
+++ Ejercicios/Ejercicio-Tarea7/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "menu.h"
+#include "productos.h"
+#include "factura.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+	menu();
+	
+	return 0;
+}
