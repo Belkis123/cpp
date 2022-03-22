9f978c00dd42701a43163d153beb8115e18396a6
Belkis Tejada <ptejada153@gmail.com>
2022-03-22 00:10:12 -0700

    Agregando Tarea8

---
+++ Ejercicios/Tarea8/raizcuadrada.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+  float numero,raiz;
+ 
+ 
+  cout << "Calcular la Raiz Cuadrada" <<endl;
+  cout <<endl;
+  cout << "	Ingrese un numero para obtener la raiz cuadrada:";
+  cin >> numero;
+ 
+  raiz = sqrt(numero);
+ 
+  cout << "La raiz cuadrada de " << numero << " es : " << raiz << endl;
+ 
+  return 0;
+}
