fc7c568aac59d7b2addabdd57028121fb9748d0c
Belkis Tejada <ptejada153@gmail.com>
2022-03-27 18:56:12 -0700

    Agregando Tarea9

---
+++ Ejercicios/Tarea9/lanzar dados.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int dado1,dado2;
+string lanzar;
+
+
+int main()
+{
+    while (true)
+    {
+    	cout<<"                          Juego de Dados                      "<<endl;
+        cout<<"Se gana con los numeros 2, 4 , y se pierden con los numeros 6 y 8 "<<endl;
+		
+		
+		
+        cout<<"Lanzar los dados: "<<endl;
+        cin>>lanzar;
+        
+        system ("cls");
+        for (int i=0;i<=1;i++)
+        {
+            srand (time (0));
+            dado1=rand()%(6)+1;
+            dado2=rand()%(6)+1; 
+        }
+        cout<<"Dado 1 ["<<dado1<<"]";
+        cout<<" Dado 2 ["<<dado2<<"]"<<endl;
+        cout<<"La suma de los dados es: ["<<dado1+dado2<<"]"<<endl;
+        if (dado1+dado2 == 2 or dado1+dado2 == 4 )
+        {
+            cout<<endl;
+			cout<<"El jugador gana, la casa pierde "<<endl;
+            break;
+        }
+        if (dado1+dado2 == 6 or dado1+dado2 == 8)
+        {
+            cout<<endl;
+			cout<<"La casa gana, el jugador pierde "<<endl;
+            break;
+        }
+        
+		cout<<endl;
+        cout<<"Vuelve a lanzar "<<endl;
+        
+         
+        system ("pause");
+        system ("cls");
+    }   
+}
