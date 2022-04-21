480179f90be4034db5900480cc6d2b42f316656d
Belkis Tejada <ptejada153@gmail.com>
2022-04-20 22:12:06 -0700

    Agregando Tarea11

---
+++ Ejercicios/Proyecto/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "seguridad.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	bool respuesta = login  ();
+	
+	if (respuesta == true) {
+		cout << "Bienvenido al Sistema";
+	}
+	
+	return 0;
+	
+}
--- Ejercicios/Tarea11/main.cpp
+++ Ejercicios/Tarea11/main.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
-
-using namespace std;
-
-int main(int argc, char const *argv[])
-{
-    string nombres [5] [2];
-
-    nombres [0][0] = "Juan";
-    nombres [0][1] = "Perez";
-
-    nombres [1][0] = "Maria";
-    nombres [1][1] = "Martinez";
-
-    nombres [2][0] = "Pedro";
-    nombres [2][1] = "Jimenez";
-    
-    nombres [3][0] = "Rosa";
-    nombres [3][1] = "Garcia";
-    
-    nombres [4][0] = "Pablo";
-    nombres [4][1] = "Mejia";
-
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;  
  
-   for (int i = 0; i < 5; i++)
-   {
-       for (int j = 0; j < 2; j++)
-       {
-          cout << nombres [i][j] <<" ";
-       }
-       cout << endl;
-       
-   }
-   
+int random();
+ 
+ 
+int main()
+{
+    int filas = 5;
+	int columnas = 5; 
+	int  TotalSuma = 0;
+	int s;
+    srand(time(NULL));
+    
+ 
+    int matriz[filas][columnas];
+ 
+    for (int i = 0; i < filas; i++)
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = random();
+            TotalSuma += matriz[i][j];
+        }
+ 
+ 
+
+    cout<<" \n Sumar todos los elementos de un arreglo bidimensional"<<endl;
+	cout<<endl;
+	
+      
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf(" %2d  ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+ 
+    printf("\n TotalSuma: %d\n", TotalSuma);
+ 
     return 0;
-}
+	}
+	 
+	int random()
+	{
+	    return rand() % 10 + 1;
+	}
