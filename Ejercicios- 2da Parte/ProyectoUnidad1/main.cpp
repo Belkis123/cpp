4da5b2ad648c0ec10ea1c8beb1b924c035be8bf7
Belkis Tejada <ptejada153@gmail.com>
2022-06-18 18:52:04 -0600

    .

--- Ejercicios- 2da Parte/ProyectoUnidad1/main.cpp
+++ Ejercicios- 2da Parte/ProyectoUnidad1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "startShip.h"
 #include "snake.h"
+#include <string>
 
 using namespace std;
 
@@ -10,9 +11,52 @@ int main(int argc, char const *argv[])
 
      startShip();
      snake();
-
      
+ };
 
+ int main(int argc, char const *argv[])
+{
+    int opcion = 0;
 
-	 return 0;
+    while (true) {
+            
+        system ("cls");
+
+        cout << endl;
+        cout << "****************" << endl;
+        cout << "* MENU DE JUEGOS *" << endl;
+        cout << "****************" << endl;
+        cout << endl;
+        cout << " ELIGE UN JUEGO! " << endl;
+        cout << endl;
+        cout << "1 - starTShip" << endl;
+        cout << "2 - snake" << endl;
+        cout << "0 - Salir" << endl;
+        cout << endl;
+        cout << "Ingrese una opcion del menu de Juegos: ";
+        cin >> opcion;
+        
+        switch (opcion)
+        {
+        case 1:
+            startShip();
+            break;
+        
+        case 2:
+            snake();
+            break;
+         
+        default:
+        break;
+        }
+
+        system ("pause");
+        cout << endl;
+        if (opcion == 0)
+        {
+            break;
+        }
+    }
+
+    return 0;
 }
